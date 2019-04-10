61fd7e8c33a53e8692880e791cc0c68a2288e590
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-10 08:17:38 -0500

    Updates

---
+++ precache-manifest.6dd979f88e46666be224adc7f8eaa7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/api-youtube-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7694bdaf623c5f1d6873",
+    "url": "/api-youtube-react/static/js/runtime~main.b47334df.js"
+  },
+  {
+    "revision": "b84eccdbc68cb62bd823",
+    "url": "/api-youtube-react/static/js/main.f5f904d4.chunk.js"
+  },
+  {
+    "revision": "57672416cac49815327e",
+    "url": "/api-youtube-react/static/js/2.7f6d5229.chunk.js"
+  },
+  {
+    "revision": "b84eccdbc68cb62bd823",
+    "url": "/api-youtube-react/static/css/main.333896fd.chunk.css"
+  },
+  {
+    "revision": "afb31f7f7d8f6244fd84e8aa2320ebf0",
+    "url": "/api-youtube-react/index.html"
+  }
+];
